13fd76283bce8cbb15ed73484cfec39ee7b02ea4
Yves Takaoka <yves@rumotecnologia.com.br>
2022-07-05 16:08:21 -0300

    Create custom_grid_tile_bar.dart
    Custom Grid Tile Bar.

---
+++ lib/widgets/custom_grid_tile_bar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class GridTileBar2 extends StatelessWidget {
+  const GridTileBar2({
+    Key? key,
+    this.backgroundColor,
+    this.leading,
+    this.title,
+    this.subtitle,
+    this.trailing,
+  }) : super(key: key);
+
+  final Color? backgroundColor;
+  final Widget? leading;
+  final Widget? title;
+  final Widget? subtitle;
+  final Widget? trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    BoxDecoration? decoration;
+    if (backgroundColor != null) decoration = BoxDecoration(color: backgroundColor);
+
+    final ThemeData darkTheme = ThemeData.dark();
+    return Container(
+      decoration: decoration,
+      height: (title != null && subtitle != null) ? 68.0 : 33.0,
+      child: Theme(
+        data: darkTheme,
+        child: IconTheme.merge(
+          data: const IconThemeData(color: Colors.white),
+          child: Row(
+            children: <Widget>[
+              if (leading != null) Container(child: leading),
+              if (title != null && subtitle != null)
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      DefaultTextStyle(
+                        style: darkTheme.textTheme.subtitle1!,
+                        softWrap: false,
+                        overflow: TextOverflow.ellipsis,
+                        child: title!,
+                      ),
+                      DefaultTextStyle(
+                        style: darkTheme.textTheme.caption!,
+                        softWrap: false,
+                        overflow: TextOverflow.ellipsis,
+                        child: subtitle!,
+                      ),
+                    ],
+                  ),
+                )
+              else if (title != null || subtitle != null)
+                Expanded(
+                  child: DefaultTextStyle(
+                    style: darkTheme.textTheme.subtitle1!,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    child: title ?? subtitle!,
+                  ),
+                ),
+              if (trailing != null) Container(child: trailing),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
