9d4310606d48432e1edb9342136d1d81cb255409
Yves Takaoka <yves@rumotecnologia.com.br>
2022-07-05 16:07:25 -0300

    Update products_provider.dart
    Fetching and setting data from Firebase.

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -4,35 +4,35 @@ import 'product.dart';
 import 'package:http/http.dart' as http;
 
 class ProductsProvider with ChangeNotifier {
-  final List<Product> _items = [
-    Product(
-      id: "1",
-      title: "Mouse",
-      description: "Mouse for computers",
-      price: 99.99,
-      imageUrl: "https://cdn.pixabay.com/photo/2017/05/24/21/33/workplace-2341642__340.jpg",
-    ),
-    Product(
-      id: "2",
-      title: "Keyboard",
-      description: "Keyboard for computers",
-      price: 199.99,
-      imageUrl: "https://cdn.pixabay.com/photo/2015/12/15/19/35/keyboard-1094833__340.jpg",
-    ),
-    Product(
-      id: "3",
-      title: "iPad",
-      description: "Last generation iPad",
-      price: 499.99,
-      imageUrl: "https://cdn.pixabay.com/photo/2019/10/07/10/40/ipad-4532326_960_720.jpg",
-    ),
-    Product(
-      id: "4",
-      title: "MacBook",
-      description: "Last generation MacBook",
-      price: 1999.99,
-      imageUrl: "https://cdn.pixabay.com/photo/2016/10/11/09/26/office-1730940__340.jpg",
-    ),
+  List<Product> _items = [
+    // Product(
+    //   id: "1",
+    //   title: "Mouse",
+    //   description: "Mouse for computers",
+    //   price: 99.99,
+    //   imageUrl: "https://cdn.pixabay.com/photo/2017/05/24/21/33/workplace-2341642__340.jpg",
+    // ),
+    // Product(
+    //   id: "2",
+    //   title: "Keyboard",
+    //   description: "Keyboard for computers",
+    //   price: 199.99,
+    //   imageUrl: "https://cdn.pixabay.com/photo/2015/12/15/19/35/keyboard-1094833__340.jpg",
+    // ),
+    // Product(
+    //   id: "3",
+    //   title: "iPad",
+    //   description: "Last generation iPad",
+    //   price: 499.99,
+    //   imageUrl: "https://cdn.pixabay.com/photo/2019/10/07/10/40/ipad-4532326_960_720.jpg",
+    // ),
+    // Product(
+    //   id: "4",
+    //   title: "MacBook",
+    //   description: "Last generation MacBook",
+    //   price: 1999.99,
+    //   imageUrl: "https://cdn.pixabay.com/photo/2016/10/11/09/26/office-1730940__340.jpg",
+    // ),
   ];
 
   List<Product> get items {
@@ -47,27 +47,61 @@ class ProductsProvider with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
+  Future<void> fetchAndSetProducts() async {
+    final url = Uri.parse('https://myshop-f4884-default-rtdb.firebaseio.com/products.json');
+    try {
+      final response = await http.get(url);
+      final extractedData = jsonDecode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractedData.forEach(
+        (prodId, prodData) {
+          loadedProducts.add(
+            Product(
+              id: prodId,
+              title: prodData['title'],
+              description: prodData['description'],
+              price: prodData['price'],
+              imageUrl: prodData['imageUrl'],
+              isFavorite: prodData['isFavorite'],
+            ),
+          );
+        },
+      );
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+      //todo: add error handling
+    }
+  }
+
   Future<void> addProduct(Product product) async {
     final url = Uri.parse('https://myshop-f4884-default-rtdb.firebaseio.com/products.json');
-    final response = await http.post(url,
+    try {
+      final response = await http.post(
+        url,
         body: json.encode(
           {
             'title': product.title,
             'description': product.description,
-            'imagerUrl': product.imageUrl,
+            'imageUrl': product.imageUrl,
             'price': product.price,
             'isFavorite': product.isFavorite,
           },
-        ));
-    final newProduct = Product(
-      id: json.decode(response.body)['name'],
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
-    notifyListeners();
+        ),
+      );
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
   }
 
   void updateProduct(String id, Product product) {
