7a611a3275e25f149be45a20a42db67f20bee2ee
Yves Takaoka <yves@rumotecnologia.com.br>
2022-07-05 16:08:45 -0300

    Update product_item.dart
    Improvements on the Product Tile at Product Over View page.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_shop/widgets/custom_grid_tile_bar.dart';
 import '../providers/cart.dart';
 import '../screens/product_detail.dart';
 import 'package:provider/provider.dart';
@@ -14,47 +15,61 @@ class ProductItem extends StatelessWidget {
     final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
-          },
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
-        ),
-        footer: GridTileBar(
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.black54,
-          leading: Consumer<Product>(
-            builder: (ctx, product, _) => IconButton(
-              icon: product.isFavorite ? const FaIcon(FontAwesomeIcons.solidHeart) : const FaIcon(FontAwesomeIcons.heart),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {
-                product.toggleFavorite();
+      child: Stack(
+        children: <Widget>[
+          GridTile(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
               },
+              child: Image.network(product.imageUrl, fit: BoxFit.cover),
+            ),
+            footer: GridTileBar2(
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              ),
+              backgroundColor: Colors.black54,
+              trailing: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    cart.addItem(product.id!, product.imageUrl, product.price, product.title);
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Added item do cart!'),
+                        duration: const Duration(seconds: 2),
+                        action: SnackBarAction(
+                          label: 'UNDO',
+                          onPressed: () {
+                            cart.subtractQuantity(product.id!);
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  icon: FaIcon(FontAwesomeIcons.cartShopping, size: 16, color: Theme.of(context).colorScheme.secondary)),
             ),
           ),
-          trailing: IconButton(
-              onPressed: () {
-                cart.addItem(product.id!, product.imageUrl, product.price, product.title);
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: const Text('Added item do cart!'),
-                    duration: const Duration(seconds: 2),
-                    action: SnackBarAction(
-                      label: 'UNDO',
-                      onPressed: () {
-                        cart.subtractQuantity(product.id!);
-                      },
-                    ),
+          Positioned(
+            right: 0,
+            child: Consumer<Product>(
+              builder: (ctx, product, _) => InkWell(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+                  child: Container(
+                    color: Colors.black54,
+                    padding: const EdgeInsets.all(8),
+                    child: FaIcon(product.isFavorite ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart, size: 16, color: Colors.orangeAccent),
                   ),
-                );
-              },
-              icon: FaIcon(FontAwesomeIcons.cartShopping, color: Theme.of(context).colorScheme.secondary)),
-        ),
+                ),
+                onTap: () {
+                  product.toggleFavorite();
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
